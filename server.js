44bd4848f05998c8b2d715eb37018b0b291ee202
Aslak2322 <147713237+Aslak2322@users.noreply.github.com>
2025-04-28 10:34:20 +0200

    fixed frontend path

--- server.js
+++ server.js
@@ -33,10 +33,10 @@ app.use(passport.session());
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'frontend', 'e-commerce', 'build')));
+app.use(express.static(path.join(__dirname, 'front-end', 'e-commerce', 'build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'e-commerce', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'front-end', 'e-commerce', 'build', 'index.html'));
   });
 
 app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
