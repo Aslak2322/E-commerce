b2ca57466e0481d04ec0bf90d1b390e4cadd94a5
Aslak2322 <147713237+Aslak2322@users.noreply.github.com>
2025-04-30 11:08:16 +0200

    external uri maybe

--- db/index.js
+++ db/index.js
@@ -8,10 +8,11 @@ const pool = new Pool({
     database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
     port: process.env.PGPORT,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false // This allows the connection to Render's PostgreSQL service.
     }
-})
+});
  
 module.exports.query = (text, params) => {
   return pool.query(text, params)
