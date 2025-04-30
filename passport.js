754422489da8c1b2e1a84fe4658e6ff42cd106b5
Aslak2322 <147713237+Aslak2322@users.noreply.github.com>
2025-04-30 09:08:18 +0200

    passport

--- passport.js
+++ passport.js
@@ -6,7 +6,7 @@ const API_URL = process.env.REACT_APP_API_URL;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5001/auth/google/callback",
+    callbackURL: `${API_URL}/auth/google/callback`,
 }, async (accessToken, refreshToken, profile, done) => {
     console.log("Google Profile:", profile);
     const user =  {
